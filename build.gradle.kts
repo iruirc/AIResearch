6cbbe98e7190ec752a19ecba7aab1653fb6cd011
Dmitry Nuzhin <iruirc@gmail.com>
2025-11-04 20:56:57 +0300

    Fix JVM target compatibility between Java and Kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,17 @@ application {
     mainClass = "io.ktor.server.netty.EngineMain"
 }
 
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+    }
+}
+
 dependencies {
     implementation(libs.ktor.server.core)
     implementation(libs.ktor.server.netty)
